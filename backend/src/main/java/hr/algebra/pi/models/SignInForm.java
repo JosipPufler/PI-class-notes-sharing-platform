fe6850a99e8192ad8510dc09816cc85573734449
Josip Pufler <jpufler@algebra.hr>
2024-12-05 20:57:57 +0100

    switched to tembo

--- backend/src/main/java/hr/algebra/pi/models/SignInForm.java
+++ backend/src/main/java/hr/algebra/pi/models/SignInForm.java
@@ -1,5 +1,10 @@
 package hr.algebra.pi.models;
 
+import lombok.Getter;
+import lombok.Setter;
+
+@Setter
+@Getter
 public class SignInForm {
     String username;
     String password;
@@ -15,54 +20,6 @@ public class SignInForm {
         this.phoneNumber = phoneNumber;
         this.firstName = firstName;
         this.lastName = lastName;
-    }
-
-    public String getUsername() {
-        return username;
-    }
-
-    public void setUsername(String username) {
-        this.username = username;
-    }
-
-    public String getLastName() {
-        return lastName;
-    }
-
-    public void setLastName(String lastName) {
-        this.lastName = lastName;
-    }
-
-    public String getFirstName() {
-        return firstName;
-    }
-
-    public void setFirstName(String firstName) {
-        this.firstName = firstName;
-    }
-
-    public String getPhoneNumber() {
-        return phoneNumber;
-    }
-
-    public void setPhoneNumber(String phoneNumber) {
-        this.phoneNumber = phoneNumber;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
     }
 
     @Override
